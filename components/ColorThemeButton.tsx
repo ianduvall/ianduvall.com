29733287c9d0f006c80b7b673c967e9a61a2a938
Ian Duvall <iduvall@umich.edu>
2021-09-18 14:48:41 -0500

    Use next-themes

--- components/ColorThemeButton.tsx
+++ components/ColorThemeButton.tsx
@@ -1,52 +1,20 @@
 import React from 'react';
 import { SecondaryButton } from '@/system/Button';
 import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
-import { lightTheme, darkTheme } from 'stitches.config';
-import Script from 'next/script';
-
-const getInitialColorTheme = () => {
-  try {
-    if (typeof global.window === 'undefined') {
-      return lightTheme;
-    }
-    return document.body.className === darkTheme.className ? darkTheme : lightTheme;
-  } catch (error) {
-    console.log(error);
-  }
-
-  return lightTheme;
-};
+import { useTheme } from 'next-themes';
 
 export const ColorThemeButton = () => {
-  const themeChangedRef = React.useRef<boolean>(false);
-  const [theme, setTheme] = React.useState<typeof lightTheme | typeof darkTheme>(
-    getInitialColorTheme
-  );
-
-  React.useEffect(() => {
-    const themeChanged = themeChangedRef.current;
-    if (themeChanged) {
-      if (document.body) {
-        document.body.classList.remove(lightTheme, darkTheme);
-        document.body.classList.add(theme);
-      }
-
-      localStorage.setItem('color_theme', theme.className);
-    }
-  }, [theme]);
+  const { theme, setTheme } = useTheme();
+  if (!theme) return null;
 
   return (
-    <>
-      <Script src="/determineColorMode.js" strategy="beforeInteractive"></Script>
-      <SecondaryButton
-        aria-label={theme === darkTheme ? 'Use light color theme' : 'Use dark color theme'}
-        onClick={() => {
-          themeChangedRef.current = true;
-          setTheme((prevTheme) => (prevTheme === lightTheme ? darkTheme : lightTheme));
-        }}
-      >
-        {theme === darkTheme ? <SunIcon aria-hidden /> : <MoonIcon aria-hidden />}
-      </SecondaryButton>
-    </>
+    <SecondaryButton
+      aria-label={theme === 'dark' ? 'Use light color theme' : 'Use dark color theme'}
+      onClick={() => {
+        setTheme(theme === 'light' ? 'dark' : 'light');
+      }}
+    >
+      {theme === 'dark' ? <SunIcon aria-hidden /> : <MoonIcon aria-hidden />}
+    </SecondaryButton>
   );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,31 +6,40 @@ import { useAnalytics } from '@/lib/analytics';
 import { Footer } from '@/components/Footer';
 import { globalStyles } from '@/styles/global';
 import { Box } from '@/system';
+import { ThemeProvider as ColorThemeProvider } from 'next-themes';
+import { lightTheme, darkTheme } from 'stitches.config';
+
+const colorThemeProviderValue = {
+  light: lightTheme.className,
+  dark: darkTheme.className,
+};
 
 function App({ Component, pageProps }: AppProps) {
   globalStyles();
   useAnalytics();
 
   return (
-    <IdProvider>
-      <Head>
-        <title>Ian Duvall</title>
-      </Head>
+    <ColorThemeProvider attribute="class" value={colorThemeProviderValue}>
+      <IdProvider>
+        <Head>
+          <title>Ian Duvall</title>
+        </Head>
 
-      <Box
-        css={{
-          display: 'flex',
-          minHeight: '100vh',
-          flexDirection: 'column',
-        }}
-      >
-        <Box css={{ flex: 1 }}>
-          <Component {...pageProps} />
+        <Box
+          css={{
+            display: 'flex',
+            minHeight: '100vh',
+            flexDirection: 'column',
+          }}
+        >
+          <Box css={{ flex: 1 }}>
+            <Component {...pageProps} />
+          </Box>
+
+          <Footer />
         </Box>
-
-        <Footer />
-      </Box>
-    </IdProvider>
+      </IdProvider>
+    </ColorThemeProvider>
   );
 }
 
