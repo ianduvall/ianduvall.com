972c26e86e9d1b7bf0ece7731aeeda55a48c2621
Ian Duvall <iduvall@umich.edu>
2023-02-11 19:22:18 -0600

    Moving bits around

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = "Ian Duvall";
+export const SITE_DESCRIPTION = "Ian Duvall's personal website";
+export const TWITTER_USERNAME = "ianwmduvall";
+export const GITHUB_USERNAME = "ianduvall";
