2927e7f29090d2de9f2bd29bfd0b5f9cb9f0df50
Ian Duvall <iduvall@umich.edu>
2024-02-18 10:08:31 -0600

    Reset

--- README.md
+++
@@ -1,1 +0,0 @@
-# ianduvall.com
--- src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const SITE_TITLE = "Ian Duvall";
-export const SITE_DESCRIPTION = "Ian Duvall's personal website";
-export const TWITTER_USERNAME = "ianwmduvall";
-export const GITHUB_USERNAME = "ianduvall";
