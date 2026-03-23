6244b90a95493b0ef4adaebe8e9cabf8a4a42188
Ian Duvall <iduvall@umich.edu>
2026-03-23 14:29:18 -0700

    bump prompt api origin trial header

--- next.config.ts
+++ next.config.ts
@@ -18,7 +18,7 @@ const nextConfig: NextConfig = {
 					{
 						key: "Origin-Trial",
 						value:
-							"AkICwQrePfsq4kJB3XCrh9w0gI+YP6ttvF3yt/cq0/lD5i6IV+Qnx0RUnde4g6Wpm32o1MkraEhDJ8Aun9dyIgsAAAB4eyJvcmlnaW4iOiJodHRwczovL3d3dy5pYW5kdXZhbGwuY29tOjQ0MyIsImZlYXR1cmUiOiJBSVByb21wdEFQSU11bHRpbW9kYWxJbnB1dCIsImV4cGlyeSI6MTc3NDMxMDQwMCwiaXNTdWJkb21haW4iOnRydWV9",
+							"AnUtC3f8dm0x5YMUfecaMcP96eNbaD2alTKIIVCpOC5cJcwYN+wpbBUy/zCNnwL25wBT5tQWUIy+h7+/6FPSXwoAAAB4eyJvcmlnaW4iOiJodHRwczovL3d3dy5pYW5kdXZhbGwuY29tOjQ0MyIsImZlYXR1cmUiOiJBSVByb21wdEFQSU11bHRpbW9kYWxJbnB1dCIsImV4cGlyeSI6MTc4MTU2ODAwMCwiaXNTdWJkb21haW4iOnRydWV9",
 					},
 				],
 			},
